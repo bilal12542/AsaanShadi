f5a8bd182c3c5428eaf52694383d1fdbd133118d
Noor Aftab <58308273+nooraftab779@users.noreply.github.com>
2021-12-23 13:02:29 +0500

    Initial Push

--- service_provider/admin.py
+++ service_provider/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Photography,Transportation,Decoration,Catering
+
+
+admin.site.register(Photography)
+admin.site.register(Transportation)
+admin.site.register(Decoration)
+admin.site.register(Catering)
+
--- service_provider/models.py
+++ service_provider/models.py
@@ -1,3 +1,57 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Catering(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, default="")
+    location = models.CharField(max_length=150, default="")
+    contact_no = models.CharField(max_length=100 , default="")
+    desc = models.CharField(max_length=400 ,  default="")
+    package = models.CharField(max_length=100, default="")
+    price = models.CharField(max_length=130 , default="")
+
+    def __str__(self):
+        return self.company_name
+
+
+class Decoration(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, default="")
+    location = models.CharField(max_length=150, default="")
+    contact_no = models.CharField(max_length=100, default="")
+    desc = models.CharField(max_length=400, default="")
+    package = models.CharField(max_length=100, default="")
+    price = models.CharField(max_length=130, default="")
+
+    def __str__(self):
+        return self.company_name
+
+class Transportation(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, default="")
+    location = models.CharField(max_length=150, default="")
+    contact_no = models.CharField(max_length=100, default="")
+    desc = models.CharField(max_length=400, default="")
+    package = models.CharField(max_length=100, default="")
+    price = models.CharField(max_length=130, default="")
+
+    def __str__(self):
+        return self.company_name
+
+
+class Photography(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, default="")
+    location = models.CharField(max_length=150, default="")
+    contact_no = models.CharField(max_length=100, default="")
+    desc = models.CharField(max_length=400, default="")
+    package = models.CharField(max_length=100, default="")
+    price = models.CharField(max_length=130, default="")
+
+    def __str__(self):
+        return self.company_name
+
+
+
