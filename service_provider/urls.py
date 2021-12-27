ba9d73fc495289e57e99eb86396e41dd70b01e0d
Noor Aftab <58308273+nooraftab779@users.noreply.github.com>
2021-12-27 17:10:49 +0500

    3rd service push

--- service_provider/urls.py
+++ service_provider/urls.py
@@ -9,9 +9,19 @@ urlpatterns = [
     path("cateringpage/add_Category", views.add_Cateogry, name="categoryAdd"),
     path("cateringpage/<int:id>", views.detail_catering, name="detail-catering"),
     path('cateringpage/<int:id>/delete', views.catering_delete_view, name= "delete-catering"),
+    path('cateringpage/<int:id>/update', views.update_Service, name= "update-catering"),
     path("decorationpage", views.decorations, name="decorations"),
+    path("decorationpage/<int:id>", views.detail_decorations, name="detail-decorations"),
+    path('decorationpage/<int:id>/delete', views.decorations_delete_view, name= "delete-decorations"),
+
     path("photographypage", views.photography, name="photography"),
+    path("photographypage/<int:id>", views.detail_photography, name="detail-photography"),
+    path('photographypage/<int:id>/delete', views.photography_delete_view, name="delete-photography"),
+
     path("transportpage", views.transport, name="transport"),
+    path("transportpage/<int:id>", views.detail_transport, name="detail-transport"),
+    path('transportpage/<int:id>/delete', views.transport_delete_view, name="delete-transport"),
+
 
     path("cateringpage/add_Package", views.add_Package, name="PackageAdd"),
     path("cateringpage/add_Service" , views.add_Service,name="ServiceAdd")
--- service_provider/views.py
+++ service_provider/views.py
@@ -6,8 +6,10 @@ from django.contrib import messages
 
 
 def index(request):
-    data = 123
-    return render(request, 'service_provider/index.html', {"data":data})
+    context = {}
+    context["dataset"] = ServiceProvider.objects.all()
+    print(context)
+    return render(request, 'service_provider/index.html', context)
 
 def about(request):
     return render(request, 'service_provider/about_page.html')
@@ -61,6 +63,38 @@ def decorations(request):
 
     return render(request, 'service_provider/decorations.html',context)
 
+
+def detail_decorations(request, id):
+    context = {}
+    cat = Category.objects.get(category="Decorations")
+    cat_service = ServiceProvider.objects.filter(category=cat.id)
+    context["data"] = cat_service.get(id=id)
+
+
+    # add the dictionary during initialization
+
+    return render(request, "service_provider/detail_view.html", context)
+
+def decorations_delete_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+
+    # fetch the object related to passed id
+    cat = Category.objects.get(category="Decorations")
+    cat_service = ServiceProvider.objects.filter(category=cat.id)
+    obj = get_object_or_404(cat_service, id=id)
+
+    if request.method == "POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to
+        # home page
+        return redirect("catering")
+
+    return render(request, "service_provider/delete_view.html", context)
+
+
 def photography(request):
     cat = Category.objects.get(category="Photography")
 
@@ -69,6 +103,37 @@ def photography(request):
 
     return render(request, 'service_provider/photography.html',context)
 
+
+def detail_photography(request, id):
+    context = {}
+    cat = Category.objects.get(category="Photography")
+    cat_service = ServiceProvider.objects.filter(category=cat.id)
+    context["data"] = cat_service.get(id=id)
+
+
+    # add the dictionary during initialization
+
+    return render(request, "service_provider/detail_view.html", context)
+
+def photography_delete_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+
+    # fetch the object related to passed id
+    cat = Category.objects.get(category="Photography")
+    cat_service = ServiceProvider.objects.filter(category=cat.id)
+    obj = get_object_or_404(cat_service, id=id)
+
+    if request.method == "POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to
+        # home page
+        return redirect("catering")
+
+    return render(request, "service_provider/delete_view.html", context)
+
 def transport(request):
     cat = Category.objects.get(category="Transport")
 
@@ -76,6 +141,36 @@ def transport(request):
     context["dataset"] = ServiceProvider.objects.filter(category=cat.id)
 
     return render(request, 'service_provider/transport.html',context)
+
+def detail_transport(request, id):
+    context = {}
+    cat = Category.objects.get(category="Transport")
+    cat_service = ServiceProvider.objects.filter(category=cat.id)
+    context["data"] = cat_service.get(id=id)
+
+
+    # add the dictionary during initialization
+
+    return render(request, "service_provider/detail_view.html", context)
+
+def transport_delete_view(request, id):
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+
+    # fetch the object related to passed id
+    cat = Category.objects.get(category="Transport")
+    cat_service = ServiceProvider.objects.filter(category=cat.id)
+    obj = get_object_or_404(cat_service, id=id)
+
+    if request.method == "POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to
+        # home page
+        return redirect("catering")
+
+    return render(request, "service_provider/delete_view.html", context)
 
 
 def add_Package(request):
@@ -111,13 +206,33 @@ def add_Service(request):
         service_form = ServiceForm(request.POST,request.FILES)
         #print(service_form)
         if service_form.is_valid():
-            print("1")
             service_form.save()
             messages.success(request, "Successfully added!")
             return redirect('catering')
         else:
             print(service_form.errors)
     else:
-        print("I'm done ")
         service_form = ServiceForm()
     return render(request, "service_provider/add_Service.html", {'form':service_form} )
+
+def update_Service(request,id):
+    # or "Decortaions" or "Photography" or "Transport"
+    # cat = Category.objects.get(category="Catering" )
+    # cat_service = ServiceProvider.objects.filter(category=cat.id)
+    obj = get_object_or_404(ServiceProvider, id=id)
+    #print(cat)
+
+    print(obj.location)
+    context = {}
+    # add the dictionary during initialization
+    service_form = ServiceForm(request.POST or None, request.FILES or None , instance = obj)
+    print(service_form)
+    if service_form.is_valid():
+        service_form.save()
+        messages.success(request, "Successfully Updated!")
+        return redirect('HomePage')
+    else:
+        pass
+        #print(service_form.errors)
+    context["form"] = service_form
+    return render(request, "service_provider/new_update.html", context )
