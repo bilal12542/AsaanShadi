d6c793d11fb20dcdfd97d8ccd38404476d54dc5c
bilal12542 <bilal.12542@gmail.com>
2021-12-28 16:38:58 +0500

    tuesday

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
@@ -8,9 +8,19 @@ urlpatterns = [
                   path('become-vendor/', views.become_vendor, name='become-vendor'),
                   path('dashboard/', views.vendor_dashboard, name='dashboard'),
                   path('add-product/', views.add_product, name='add-product'),
-                  path('view-product/', views.view_product, name='view-product'),
+                  path('view-product/', include(
+                      [
+                          path('', views.view_product, name='view-product'),
+                          path('<slug:category_slug>/<slug:product_slug>/', include(
+                              [
+                                  path('view/', views.single_product, name='single-product'),
+                                  path('edit/', views.edit_product, name='edit-product'),
+                                  path('delete/', views.delete_product, name='delete-product')
+                              ]
+                          )),
+                      ]
+                  )),
                   path('become-vendor/', views.become_vendor, name='become-vendor'),
                   path('', views.user_login, name='login'),
-                  path('<slug:category_slug>/<slug:product_slug>/', views.single_product, name='single-product'),
                   path('logout/', views.logout_vendor, name='logout'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
