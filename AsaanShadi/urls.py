29afda16eb94274143c7339ad386eea64540cbe0
Noor Aftab <58308273+nooraftab779@users.noreply.github.com>
2021-12-28 15:59:39 +0500

    new Update

--- AsaanShadi/urls.py
+++ AsaanShadi/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', include('vendor.urls')),
-              ]
+                  path('vendor/', include('vendor.urls')),
+                path('services/', include('service_provider.urls')),
+              ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
