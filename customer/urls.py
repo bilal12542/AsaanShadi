039f65e4cf671e3458b46d8c5fe1a6408f3fa550
Noor Aftab <58308273+nooraftab779@users.noreply.github.com>
2021-12-30 16:34:01 +0500

    new Update customer 2

--- customer/urls.py
+++ customer/urls.py
@@ -5,6 +5,5 @@ import service_provider.views
 urlpatterns = [
     path('', views.index , name='index'),
     path('ShowServices/' , views.show_ServiceProvider , name='show_ServiceProvider'),
-    #path('')
-    path('ShowServices/services/dashboard/<int:id>', service_provider.views.details , name='showing_ServiceProvider')
+    path('ShowServices/services/dashboard/<int:id>', service_provider.views.details , name='showing_ServiceProvider'),
 ]
